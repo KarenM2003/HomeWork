9b89e83c6e26a7149dfadd927c21aa2948a612b8
KarenM2003 <markosyankaren066@gmail.com>
2024-04-30 17:05:11 +0400

    Done all

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,1 +1,26 @@
-﻿//barev
+﻿using System.IO;
+
+string path = @"C:\Users\user\Desktop\HomeWorke\example.txt";
+string path2 = @"C:\Users\user\Desktop\HomeWorke2\example.txt";
+
+
+string text = "Barev dzez";
+
+using (StreamWriter sw = new StreamWriter(path))
+{
+    sw.WriteLine(text);
+}
+
+
+using (StreamReader sr = new StreamReader(path))
+{
+    string line;
+    using (StreamWriter sw = new StreamWriter(path2))
+    {
+        while ((line = sr.ReadLine()) != null)
+        {
+            sw.WriteLine(line);
+        }
+    }
+}
+
